094ec6b5cbac9821f2e4502e59e48b833376cde1
Andy Brett <andy@andybrett.com>
2017-11-11 12:14:36 +0545

    rename landing viewcontroller

--- Send As Post/ConfigViewController.swift
+++ Send As Post/ConfigViewController.swift
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConfigViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
