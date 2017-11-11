8f00e5cfba2514ede1cda714d6e54e11c8359068
Andy Brett <andy@andybrett.com>
2017-11-11 12:12:47 +0545

    Initial Commit

---
+++ Send As Post/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Send As Post
+//
+//  Created by Andy Brett on 11/11/17.
+//  Copyright © 2017 APB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
